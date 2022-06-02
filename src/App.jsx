70bdf24266489ac23ee8e9b2484ab05b2a2c152d
ezequiel magi <ezequielmaggi@gmail.com>
2022-06-02 10:08:41 -0300

    iniciando v1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './app.css'
+
+function App() {
+ 
+
+  return (
+      <div className='app-wrapper'>
+        <img src={'https://i.postimg.cc/RCnGsnkP/ITCpng.png'} alt="" />
+        <p>Ayudanos a mejorar tu experiencia en clases completando la siguiente encuesta:</p>
+        <button><a href="https://form.typeform.com/to/lpROXHCg">ENCUESTA</a></button>
+      </div>
+  )
+}
+
+export default App
